3595d5096c578780a0b1b47c97ec680fe80ea9fe
Devan Patel <=>
2018-05-25 16:21:30 -0400

    Training Implemented

---
+++ UnityIndoorEstimote/Assets/Scripts/Training/TrainingButtonSelect.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrainingButtonSelect : MonoBehaviour {
+
+	
+	public Canvas trainingCanvas; 
+	public Feedback_Voice voice; 
+	public Feedback_Ping ping; 
+
+	private Instruction instruction; 
+
+	void Start()
+	{
+		instruction = FindObjectOfType<Instruction>(); 
+	}
+
+
+	public void OnVoiceSelect()
+	{
+		voice.toggle.isOn = true; 
+		trainingCanvas.enabled = false; 
+		instruction.PlayInstruction(InstructionType.Voice); 
+	}
+
+	public void OnPingSelect()
+	{
+		ping.toggle.isOn = true; 
+		trainingCanvas.enabled = false; 
+		instruction.PlayInstruction(InstructionType.Ping); 
+	}
+}
